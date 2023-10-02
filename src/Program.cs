ff68636a350b7210f7f7fb83dd89e9db7c5ee552
Gilnei Gonçalves Nogueira <44248074+GilneiNogueira73@users.noreply.github.com>
2023-10-02 16:00:49 -0300

    examples

--- src/Program.cs
+++ src/Program.cs
@@ -10,7 +10,7 @@ builder.Logging.AddSerilog();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//Log.Logger = logger;
+Log.Logger = logger;
 
 var app = builder.Build();
 
@@ -20,7 +20,57 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/greet", () => "hello there")
-.WithName("hi");
+app.MapGet("/greet", () => "hello there");
+
+app.MapGet("/bug", () =>
+{
+    try
+    {
+        var z = 0;
+        var error = 1 / z;
+    }
+    catch (Exception e)
+    {
+        logger.Error($"Erro ao executar a função: {e.Message}");
+        return Results.BadRequest("Not OK");
+    }
+    return Results.Ok("OK");
+});
+
+app.MapGet("/lazy", () =>
+{
+    var x = 0;
+    try
+    {
+        var z = 10;
+        var y = 20;
+        Thread.Sleep(5000);
+        x = z * y;
+    }
+    catch (Exception e)
+    {
+        logger.Error($"Erro ao executar a função: {e.Message}");
+        return Results.BadRequest("Not OK");
+    }
+    return Results.Ok($"O resultado da função é: {x}");
+});
+
+app.MapGet("/greetAnotherServer", async () => {
+    try
+    {
+        var client = new HttpClient();
+        var response = await client.GetAsync("http://hiperdev-api-rodrigo-service/hello");
+        var answer = response.Content.ReadAsStringAsync().Result;
+
+        logger.Information($"O servidor respondeu: {answer}");
+    }
+    catch (Exception e)
+    {
+        logger.Error("Falha ao tentar cumprimentar o servidor");
+        return Results.BadRequest(e.Message);
+    }
+    return Results.Ok("OK");
+
+});
 
 app.Run();
