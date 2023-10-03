fc7d5def12d307aceb6d1cba9a1a3c3b983b6b2a
Gilnei Gonçalves Nogueira <44248074+GilneiNogueira73@users.noreply.github.com>
2023-10-03 18:26:44 -0300

    hiper-dev

--- src/Program.cs
+++ src/Program.cs
@@ -1,3 +1,4 @@
+using NewRelic.Api.Agent;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,7 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 Log.Logger = logger;
-
+Random r = new Random();
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -37,40 +38,86 @@ app.MapGet("/bug", () =>
     return Results.Ok("OK");
 });
 
-app.MapGet("/lazy", () =>
-{
-    var x = 0;
-    try
-    {
-        var z = 10;
-        var y = 20;
-        Thread.Sleep(5000);
-        x = z * y;
-    }
-    catch (Exception e)
-    {
-        logger.Error($"Erro ao executar a função: {e.Message}");
-        return Results.BadRequest("Not OK");
-    }
-    return Results.Ok($"O resultado da função é: {x}");
-});
+//app.MapGet("/lazy", () =>
+//{
+//    var x = 0;
+//    try
+//    {
+//        var z = 10;
+//        var y = 20;
+//        Thread.Sleep(10000);
+//        x = z * y;
+//    }
+//    catch (Exception e)
+//    {
+//        logger.Error($"Erro ao executar a função: {e.Message}");
+//        return Results.BadRequest("Not OK");
+//    }
+//    return Results.Ok($"O resultado da função é: {x}");
+//});
 
-app.MapGet("/greetAnotherServer", async () => {
-    try
-    {
-        var client = new HttpClient();
-        var response = await client.GetAsync("http://hiperdev-api-rodrigo-service/hello");
-        var answer = response.Content.ReadAsStringAsync().Result;
+//app.MapGet("/greetAnotherServer", async () => {
+//    try
+//    {
+//        var client = new HttpClient();
+//        var response = await client.GetAsync("http://hiperdev-api-rodrigo-service/hello");
+//        var answer = response.Content.ReadAsStringAsync().Result;
 
-        logger.Information($"O servidor respondeu: {answer}");
-    }
-    catch (Exception e)
-    {
-        logger.Error("Falha ao tentar cumprimentar o servidor");
-        return Results.BadRequest(e.Message);
-    }
-    return Results.Ok("OK");
+//        logger.Information($"O servidor respondeu: {answer}");
+//    }
+//    catch (Exception e)
+//    {
+//        logger.Error("Falha ao tentar cumprimentar o servidor");
+//        return Results.BadRequest(e.Message);
+//    }
+//    return Results.Ok("OK");
 
-});
+//});
+
+//app.MapGet("/sale", [Trace] async () => {
+//    try
+//    {
+//        var vendaId = r.Next();
+//        var codigoProduto = r.Next();
+//        var venda = new VendaDTO(vendaId, codigoProduto);
+
+//        IAgent agent = NewRelic.Api.Agent.NewRelic.GetAgent();
+//        ITransaction transaction = agent.CurrentTransaction;
+//        transaction
+//            .AddCustomAttribute("EventCode", 1)
+//            .AddCustomAttribute("ProductCode", codigoProduto)
+//            .AddCustomAttribute("SaleId", vendaId);
+
+//        var client = new HttpClient();
+//        var addItem = await client.PostAsync("http://hiperdev-api-rodrigo-service/addItemAsync", JsonContent.Create(venda));
+
+//        var addPayment = await client.PostAsync("http://hiperdev-api-rodrigo-service/addPaymentAsync", JsonContent.Create(venda));
+
+//        var closeSale = await client.PostAsync("http://hiperdev-api-rodrigo-service/completeSaleAsync", JsonContent.Create(venda));
+
+//        var answer = closeSale.Content.ReadAsStringAsync().Result;
+//        logger.Information($"Processamento da Venda: {answer}");
+//    }
+//    catch (Exception e)
+//    {
+//        logger.Error("Falha ao tentar cumprimentar o servidor");
+//        return Results.BadRequest(e.Message);
+//    }
+//    return Results.Ok("OK");
+
+//});
 
 app.Run();
+
+
+internal record VendaDTO
+{
+    public int VendaId { get; init; }
+    public int CodigoProduto { get; init; }
+
+    public VendaDTO(int vendaId, int codigoProduto)
+    {
+        VendaId = vendaId;
+        CodigoProduto = codigoProduto;
+    }
+}
