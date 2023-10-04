85e8d6e5ee99d221f9222839eb7e3261c943d84e
Gilnei Gonçalves Nogueira <44248074+GilneiNogueira73@users.noreply.github.com>
2023-10-04 16:20:13 -0300

    corrigindo bug

--- src/Program.cs
+++ src/Program.cs
@@ -27,7 +27,7 @@ app.MapGet("/bug", () =>
 {
     try
     {
-        var z = 0;
+        var z = 2;
         var error = 1 / z;
     }
     catch (Exception e)
@@ -38,74 +38,74 @@ app.MapGet("/bug", () =>
     return Results.Ok("OK");
 });
 
-//app.MapGet("/lazy", () =>
-//{
-//    var x = 0;
-//    try
-//    {
-//        var z = 10;
-//        var y = 20;
-//        Thread.Sleep(10000);
-//        x = z * y;
-//    }
-//    catch (Exception e)
-//    {
-//        logger.Error($"Erro ao executar a funcao: {e.Message}");
-//        return Results.BadRequest("Not OK");
-//    }
-//    return Results.Ok($"Resultado da funcao: {x}");
-//});
+app.MapGet("/lazy", () =>
+{
+   var x = 0;
+   try
+   {
+       var z = 10;
+       var y = 20;
+       Thread.Sleep(10000);
+       x = z * y;
+   }
+   catch (Exception e)
+   {
+       logger.Error($"Erro ao executar a funcao: {e.Message}");
+       return Results.BadRequest("Not OK");
+   }
+   return Results.Ok($"Resultado da funcao: {x}");
+});
 
-//app.MapGet("/greetAnotherServer", async () => {
-//    try
-//    {
-//        var client = new HttpClient();
-//        var response = await client.GetAsync("http://hiperdev-api-rodrigo-service/hello");
-//        var answer = response.Content.ReadAsStringAsync().Result;
+app.MapGet("/greetAnotherServer", async () => {
+   try
+   {
+       var client = new HttpClient();
+       var response = await client.GetAsync("http://hiperdev-api-rodrigo-service/hello");
+       var answer = response.Content.ReadAsStringAsync().Result;
 
-//        logger.Information($"O servidor respondeu: {answer}");
-//    }
-//    catch (Exception e)
-//    {
-//        logger.Error("Falha ao tentar cumprimentar o servidor");
-//        return Results.BadRequest(e.Message);
-//    }
-//    return Results.Ok("OK");
+       logger.Information($"O servidor respondeu: {answer}");
+   }
+   catch (Exception e)
+   {
+       logger.Error("Falha ao tentar cumprimentar o servidor");
+       return Results.BadRequest(e.Message);
+   }
+   return Results.Ok("OK");
 
-//});
+});
 
-//app.MapGet("/sale", [Trace] async () => {
-//    try
-//    {
-//        var vendaId = r.Next();
-//        var codigoProduto = r.Next();
-//        var venda = new VendaDTO(vendaId, codigoProduto);
+app.MapGet("/sale", [Trace] async () => {
+   try
+   {
+       var vendaId = r.Next();
+       var codigoProduto = r.Next();
+       var venda = new VendaDTO(vendaId, codigoProduto);
 
-//        IAgent agent = NewRelic.Api.Agent.NewRelic.GetAgent();
-//        ITransaction transaction = agent.CurrentTransaction;
-//        transaction
-//            .AddCustomAttribute("EventCode", 1)
-//            .AddCustomAttribute("ProductCode", codigoProduto)
-//            .AddCustomAttribute("SaleId", vendaId);
+       IAgent agent = NewRelic.Api.Agent.NewRelic.GetAgent();
+       ITransaction transaction = agent.CurrentTransaction;
+       transaction
+           .AddCustomAttribute("EventCode", 1)
+           .AddCustomAttribute("ProductCode", codigoProduto)
+           .AddCustomAttribute("SaleId", vendaId);
 
-//        var client = new HttpClient();
-//        var addItem = await client.PostAsync("http://hiperdev-api-rodrigo-service/addItemAsync", JsonContent.Create(venda));
+       var client = new HttpClient();
+       var addItem = await client.PostAsync("http://hiperdev-api-rodrigo-service/addItemAsync", JsonContent.Create(venda));
 
-//        var addPayment = await client.PostAsync("http://hiperdev-api-rodrigo-service/addPaymentAsync", JsonContent.Create(venda));
+       var addPayment = await client.PostAsync("http://hiperdev-api-rodrigo-service/addPaymentAsync", JsonContent.Create(venda));
 
-//        var closeSale = await client.PostAsync("http://hiperdev-api-rodrigo-service/completeSaleAsync", JsonContent.Create(venda));
+       var closeSale = await client.PostAsync("http://hiperdev-api-rodrigo-service/completeSaleAsync", JsonContent.Create(venda));
 
-//        var answer = closeSale.Content.ReadAsStringAsync().Result;
-//        logger.Information($"Processamento da Venda: {answer}");
-//    }
-//    catch (Exception e)
-//    {
-//        logger.Error("Falha ao tentar cumprimentar o servidor");
-//        return Results.BadRequest(e.Message);
-//    }
-//    return Results.Ok("OK");
+       var answer = closeSale.Content.ReadAsStringAsync().Result;
+       logger.Information($"Processamento da Venda: {answer}");
+   }
+   catch (Exception e)
+   {
+       logger.Error("Falha ao tentar cumprimentar o servidor");
+       return Results.BadRequest(e.Message);
+   }
+   return Results.Ok("OK");
 
-//});
+});
 
 app.Run();
 
